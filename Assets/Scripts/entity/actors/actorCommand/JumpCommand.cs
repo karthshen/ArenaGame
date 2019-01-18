28daed0d58240e0c6936d7296f2271b3e40c7020
karthshen <karth-shen@hotmail.com>
2019-01-18 18:14:41 -0500

    Imported InControl as the unified controller mechanism

---
+++ Assets/Scripts/entity/actors/actorCommand/JumpCommand.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpCommand : Command
+{
+    public override void Execute(AActor actor)
+    {
+        throw new System.NotImplementedException();
+    }
+}
