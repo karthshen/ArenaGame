da9c81322b5df1d44dc8adbe4ca457c2fe45b603
J. Shen <karth-shen@hotmail.com>
2019-01-18 19:34:30 -0500

    Feawture/basic command (#2)
    * Update Directories
    
    * Imported InControl as the unified controller mechanism
    
    * Basic Command structure for moving set up, and added test cases for State Pattern/Command Pattern

---
+++ Assets/Scripts/entity/actors/actorCommand/AttackCommand.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackCommand : Command
+{
+    public override void Execute(AActor actor)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/entity/actors/actorCommand/MoveCommand.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCommand : Command
+{
+    public override void Execute(AActor actor)
+    {
+        actor.Move();
+    }
+}
