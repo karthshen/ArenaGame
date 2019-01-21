81fbbddb76a47e5a2e15c2be13dbdc66d8fc46a9
karthshen <karth-shen@hotmail.com>
2019-01-21 11:45:56 -0500

    Updated Camera Structure

---
+++ Assets/Scripts/camera/CameraController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Camera arenaCamera;
+
+    float minX;
+    float maxX;
+    float minY;
+    float maxY;
+
+    Vector3 desiredPos;
+
+    float arenaCameraeraDistance;
+
+    public AActor[] actors;
+    public float cameraSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arenaCamera = GetComponent<Camera>();
+        arenaCameraeraDistance = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateCameraBounds();
+        UpdateCameraPosition();
+        MoveCamera();
+    }
+
+    private void MoveCamera()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, desiredPos, cameraSpeed);
+    }
+
+    private void UpdateCameraPosition()
+    {
+        var CONSTANT_MULTIPLIER = 0.3f;
+        var CONSTANT_CLOSE = 3f;
+
+        if (actors.Length <= 0)//early out if no actors have been found
+            return;
+        desiredPos = Vector3.zero;
+        float distance = 0f;
+        var mHeight = maxY - minY;
+        var mWidth = maxX - minX;
+        var distanceH = -(mHeight + CONSTANT_CLOSE) * CONSTANT_MULTIPLIER / Mathf.Tan(arenaCamera.fieldOfView * CONSTANT_MULTIPLIER * Mathf.Deg2Rad);
+        var distanceW = -(mWidth / arenaCamera.aspect + CONSTANT_CLOSE) * CONSTANT_MULTIPLIER / Mathf.Tan(arenaCamera.fieldOfView * CONSTANT_MULTIPLIER * Mathf.Deg2Rad);
+        distance = distanceH < distanceW ? distanceH : distanceW;
+
+        for (int i = 0; i < actors.Length; i++)
+        {
+            desiredPos += actors[i].transform.position;
+        }
+        if (distance > -CONSTANT_CLOSE) distance = -CONSTANT_CLOSE;
+        Debug.Log("Distance: "+distance);
+        desiredPos /= actors.Length;
+        desiredPos.z = distance;
+    }
+
+    private void CalculateCameraBounds()
+    {
+        minX = Mathf.Infinity;
+        maxX = -Mathf.Infinity;
+
+        minY = Mathf.Infinity;
+        maxY = -Mathf.Infinity;
+
+        foreach(AActor actor in actors){
+            Vector3 tempActor = actor.transform.position;
+            minX = tempActor.x < minX ? tempActor.x : minX;
+            maxX = tempActor.x > maxX ? tempActor.x : maxX;
+
+            minY = tempActor.y < minY ? tempActor.y : minY;
+            maxY = tempActor.y > maxY ? tempActor.y : maxY;
+        }
+    }
+
+    private void CalculateCmaeraPosAndSize()
+    {
+        Vector3 arenaCameraeraCenter = Vector3.zero;
+        Vector3 finalLookat = Vector3.zero;
+
+        foreach(AActor actor in actors)
+        {
+            arenaCameraeraCenter += actor.transform.position;
+        }
+
+        arenaCameraeraCenter = arenaCameraeraCenter / actors.Length;
+
+        //Positions arenaCameraera around a center point
+        Vector3 position = new Vector3(transform.position.x, transform.position.y, transform.position.z) + arenaCameraeraCenter;
+
+        transform.position = Vector3.Lerp(transform.position, position, cameraSpeed * Time.deltaTime);
+
+        finalLookat = Vector3.Lerp(finalLookat, arenaCameraeraCenter, cameraSpeed * Time.deltaTime);
+        //Look at
+        transform.LookAt(finalLookat);
+    }
+}
