1fe7b9c3cd1080f932b7153e768378ff66d53315
karthshen <karth-shen@hotmail.com>
2019-03-04 17:16:43 -0500

    UI Control with Gamepad

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+using UnityEngine.UI;
+
+public abstract class Menu : MonoBehaviour
+{
+    protected Button selectedButton;
+
+    public virtual void HandleInput(MenuInputHandler inputHandler, InputDevice inputDevice)
+    {
+        if (inputDevice.Action2 && selectedButton)
+        {
+            selectedButton.onClick.Invoke();
+        }
+
+        inputHandler.selector.transform.position = selectedButton.transform.position;
+    }
+}
