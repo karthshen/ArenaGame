0240b9db63abf7808a4d115a133c5a0435cce2ad
karthshen <karth-shen@hotmail.com>
2019-03-13 20:56:12 -0400

    Comment Polish

--- Assets/Scripts/entity/states/ActorState.cs
+++ Assets/Scripts/entity/states/ActorState.cs
@@ -6,7 +6,7 @@ public abstract class ActorState : EntityState
 {
     protected ActorState()
     {
-        Debug.Log("Entering State: " + GetType());
+        //Debug.Log("Entering State: " + GetType());
     }
 
     public abstract ActorState HandleInput(AActor actor, InputDevice inputDevice);
