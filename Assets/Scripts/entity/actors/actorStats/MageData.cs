db161e474a7e5b64d342924911449aa5751be1d1
karth-shen@hotmail.com <karth-shen@hotmail.com>
2019-04-07 18:25:52 -0400

    animation plus

--- Assets/Scripts/entity/actors/actorStats/MageData.cs
+++ Assets/Scripts/entity/actors/actorStats/MageData.cs
@@ -4,7 +4,7 @@
     {
         this.setActorStat(100, 5, 7, 5, 100, 5, 15, 20, 2);
 
-        AbilityDownAnimation = "animation,3";
+        AbilityDownAnimation = "animation,36";
         AbilityHorizAnimation = "animation,3";
         AbilityUpAnimation = "animation,35";
         DeathAnimation = "animation,11";
