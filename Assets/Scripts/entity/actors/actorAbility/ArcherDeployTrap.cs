7e94a226f6cb1d79b483f0187a61a2fe60d13159
karthshen <karth-shen@hotmail.com>
2019-04-06 23:15:25 -0400

    Max Number of traps

--- Assets/Scripts/entity/actors/actorAbility/ArcherDeployTrap.cs
+++ Assets/Scripts/entity/actors/actorAbility/ArcherDeployTrap.cs
@@ -5,6 +5,8 @@ public class ArcherDeployTrap : Ability
 {
     GameObject trap;
 
+    private const int MAX_NUM_TRAPS = 4;
+
     public ArcherDeployTrap(AActor caster)
     {
         this.caster = caster;
@@ -15,11 +17,22 @@ public class ArcherDeployTrap : Ability
     {
         base.AbilityExecute();
 
+        if (!IsTrapDeployable())
+            return;
+
         caster.CurrentEnergy -= AbilityCost;
 
         trap = Object.Instantiate(Resources.Load("ArcherTrap")) as GameObject;
         ArcherTrap archerTrap = trap.GetComponent<ArcherTrap>();
         archerTrap.Owner = caster;
         archerTrap.ItemStart();
+    }
+
+    private bool IsTrapDeployable()
+    {
+        ArcherTrap[] traps = GameObject.FindObjectsOfType<ArcherTrap>();
+        if (traps.Length >= MAX_NUM_TRAPS)
+            return false;
+        return true;
     }
 }
