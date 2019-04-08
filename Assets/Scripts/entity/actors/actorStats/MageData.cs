78ea628832ab0f7acbb0361cb626df8722222900
Bingyu Li <42968959+biv04@users.noreply.github.com>
2019-04-07 21:14:50 -0400

    Feature/animation debug (#31)
    * Thunder, slash, dash animation
    
    * animation plus
    
    * Boomrang, falling animation

--- Assets/Scripts/entity/actors/actorStats/MageData.cs
+++ Assets/Scripts/entity/actors/actorStats/MageData.cs
@@ -4,9 +4,9 @@
     {
         this.setActorStat(100, 5, 7, 5, 100, 5, 15, 20, 2);
 
-        AbilityDownAnimation = "animation,3";
+        AbilityDownAnimation = "animation,36";
         AbilityHorizAnimation = "animation,3";
-        AbilityUpAnimation = "animation,32";
+        AbilityUpAnimation = "animation,35";
         DeathAnimation = "animation,11";
     }
 }
