4a280a4c231fb81ea9c8d0281cb95cbf1425a658
Bingyu Li <42968959+biv04@users.noreply.github.com>
2019-04-10 11:26:59 -0400

    Feature/score board (#42)
    * Added food building
    
    * Finished texturing map 2
    
    * Score UI, 321 Count down for map2, mage teleport particle, ingame pause image

---
+++ Assets/Scripts/UI/DelayedStart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedStart : MonoBehaviour
+{
+    public GameObject countDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("StartDelay");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator StartDelay ()
+    {
+        Time.timeScale = 0;
+        float pauseTime = Time.realtimeSinceStartup + 3.5f;
+        while (Time.realtimeSinceStartup < pauseTime)
+            yield return 0;
+        countDown.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
